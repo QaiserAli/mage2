6c5bcb87ab5b16702ba92c63c233b4c77a5a5d99
QaiserAli <qaiserali.bangash@gmail.com>
2017-09-25 18:53:15 +0200

    add custom menu item to admin and link it to product catalog

--- AdminMenu/registration.php
+++ AdminMenu/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'AdminMenu',
+    'Mage2_AdminMenu',
     __DIR__
 );
