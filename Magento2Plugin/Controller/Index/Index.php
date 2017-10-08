86caa557c1b25b5e69d6c9dd159cd972c48f5ad0
QaiserAli <qaiserali.bangash@gmail.com>
2017-10-08 22:30:04 +0200

    add magento2 plugin module files

---
+++ Magento2Plugin/Controller/Index/Index.php
@@ -0,0 +1,12 @@
+<?php
+namespace Mage2\Magento2Plugin\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+
+class Index extends Action
+{
+    public function execute()
+    {
+        echo "plugin module"; exit;
+    }
+}
